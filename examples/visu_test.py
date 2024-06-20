c34a1df31b0d2d198754214cb97f20a436415256
Etienne St-Onge <stonge.etienne@gmail.com>
2024-06-20 14:41:26 -0400

    updated examples, adapted for included test files

--- examples/visu_test.py
+++ examples/visu_test.py
@@ -4,27 +4,27 @@
 import logging
 
 import trimeshpy
+from trimeshpy.data import spot, output_test_flow
 from trimeshpy.trimesh_vtk import TriMesh_Vtk
 from trimeshpy.trimeshflow_vtk import TriMeshFlow_Vtk
 
 
 def main():
-    # Test files
-    file_name = trimeshpy.data.brain_lh
-    logging.basicConfig(level=logging.DEBUG)
-
     # Load VTK surface
-    mesh = TriMesh_Vtk(file_name, None)
+    logging.basicConfig(level=logging.DEBUG)
+    mesh = TriMesh_Vtk(spot, None)
 
     # Get triangles and vertices
     triangles = mesh.get_triangles()
     vertices = mesh.get_vertices()
+    print(len(triangles))
+    print(len(vertices))
 
     # Display initial surface
     mesh.display(display_name="Trimeshpy: Initial Mesh")
 
     # pre-smooth
-    vertices = mesh.laplacian_smooth(2, 10.0, l2_dist_weighted=False, area_weighted=False, backward_step=True, flow_file=None)
+    vertices = mesh.laplacian_smooth(2, 1, l2_dist_weighted=False, area_weighted=False, backward_step=True, flow_file=None)
 
     # Update vertices from smoothed result
     mesh.set_vertices(vertices)
@@ -33,11 +33,11 @@ def main():
     mesh.display(display_name="Trimeshpy: Smoothed Mesh")
 
     # Execute Surface Flow (mass_stiffness_smooth)
-    vertices = mesh.mass_stiffness_smooth(5, 10.0, flow_file=trimeshpy.data.output_test_flow)
+    vertices = mesh.mass_stiffness_smooth(20, 0.01, flow_file=output_test_flow)
 
     # Update flow vertices from output file
     tri_mesh_flow = TriMeshFlow_Vtk(triangles, vertices)
-    tri_mesh_flow.set_vertices_flow_from_hdf5(trimeshpy.data.output_test_flow)
+    tri_mesh_flow.set_vertices_flow_from_hdf5(output_test_flow)
 
     # Display resulting surface and flow lines
     tri_mesh_flow.display(display_name="Trimeshpy: Flow resulting surface")
